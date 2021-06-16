8dc5ab3fbb8ad6b48200a6c61db6af382f740944
Jakub Królik <01142179@pw.edu.pl>
2021-06-16 08:41:45 +0200

    Added calendar and time components for futre use reservation

---
+++ src/Resources/Calendar.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import TextField from "@material-ui/core/TextField";
+import AdapterDateFns from "@material-ui/lab/AdapterDateFns";
+import LocalizationProvider from "@material-ui/lab/LocalizationProvider";
+import StaticDatePicker from "@material-ui/lab/StaticDatePicker";
+
+export default function StaticDatePickerLandscape() {
+  const [value, setValue] = React.useState<Date | null>(new Date());
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <StaticDatePicker<Date>
+        orientation="landscape"
+        openTo="day"
+        value={value}
+        onChange={(newValue) => {
+          setValue(newValue);
+        }}
+        renderInput={(params) => <TextField {...params} />}
+      />
+    </LocalizationProvider>
+  );
+}
